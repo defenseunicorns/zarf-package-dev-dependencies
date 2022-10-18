23d3593e8ec3ed19fe6c87ec67a6d6360bb2b884
Jon Perry <yrrepnoj@gmail.com>
2022-10-18 10:55:06 -0400

    more small fixes to upload script

--- upload.sh
+++ upload.sh
@@ -8,23 +8,23 @@ export CURL_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 export REQUESTS_CA_BUNDLE=/etc/ssl/certs/ca-certificates.crt
 export TWINE_CERT=/etc/ssl/certs/ca-certificates.crt
 
-ZARF_USER = $(cat /etc/zarf-state/state | jq -r '.gitServer.pushUsername')
-ZARF_PASS = $(cat /etc/zarf-state/state | jq -r '.gitServer.pushPassword')
+ZARF_USER=$(cat /etc/zarf-state/state | jq -r '.gitServer.pushUsername')
+ZARF_PASS=$(cat /etc/zarf-state/state | jq -r '.gitServer.pushPassword')
 
 #####
 # Upload all the PyPI packages
 #####
 python3 -m twine upload --repository-url=https://github.com/uploading/a/pypi \
-                       -u=redirector -p=replaces \
                        --skip-existing \
                        --verbose \
                        /packages/pypi/*  
+
 
 #####
 # Upload all the npm packages
 #####
 # Get a auth token for the npm repository
-token_response=$(curl -H "Content-Type: application/json" -d '{"name":"token-for-me-please"}' -u ${ZARF_USER}:${ZARF_PASS} http://zarf-gitea-http.zarf.svc.cluster.local:3000/api/v1/users/${ZARF_USER}/tokens)
+token_response=$(curl -H "Content-Type: application/json" -d '{"name":"token-for-me-please"}' -u ${ZARF_USER}:${ZARF_PASS} http://zarf-gitea-http.zarf.svc.cluster.local/api/v1/users/${ZARF_USER}/tokens)
 npm_token=$(echo $token_response | jq -r '.sha1')
 
 # Set the npm credentials
