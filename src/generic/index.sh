60c7892c4c5f0fd30b65bba5f413d58a1eb4731c
Wayne Starr <me@racer159.com>
2022-10-19 17:06:53 -0500

    Remove insecure flag from curl

--- src/generic/index.sh
+++ src/generic/index.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-curl -k https://github.com/defenseunicorns/zarf-package-dev-dependencies/main/packages/generic/script.sh | /bin/sh
+curl https://github.com/defenseunicorns/zarf-package-dev-dependencies/main/packages/generic/script.sh | /bin/sh
