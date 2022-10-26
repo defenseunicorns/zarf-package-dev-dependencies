19fb0b74342ec3a319f300aa1a51492bc9720f27
Wayne Starr <me@racer159.com>
2022-10-25 20:29:19 -0500

    Update node redirection

--- src/node/index.js
+++ src/node/index.js
@@ -1,7 +1,10 @@
 #!/usr/bin/env node
 
 var _ = require('lodash');
+var { VError } = require('@netflix/nerror');
 
 var hello = _.camelCase("HELLO world");
+var err = new VError('hello error! "%s"', 'le error');
 
 console.log(hello);
+console.log(err.message);
