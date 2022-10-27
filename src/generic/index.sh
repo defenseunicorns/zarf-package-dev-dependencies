6f881cb1bb96e1c5c7134c3ff1588b1766292a52
Wayne Starr <me@racer159.com>
2022-10-27 10:18:31 -0500

    Add a more realistic example for generic packages

--- src/generic/index.sh
+++ src/generic/index.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
 
-curl https://github.com/uploading/a/generic/test_packcages/script.sh/script.sh | /bin/sh
+# Install kubectl from "dl.k8s.io"
+curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
+install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+kubectl version --client
