1f70c4a18efc9c81393b65676d5e58fa27d9ceaa
Wayne Starr <me@racer159.com>
2023-01-09 14:42:39 -0600

    Add a wait for the done flag

--- src/generic/index.sh
+++ src/generic/index.sh
@@ -2,7 +2,7 @@
 
 # Install kubectl from "dl.k8s.io"
 RELEASE=$(curl -L -s https://dl.k8s.io/release/stable.txt)
-echo "Installing $RELEASE"
+echo "Installing Version $RELEASE"
 curl -LO https://dl.k8s.io/release/$RELEASE/bin/linux/amd64/kubectl
 curl -LO https://dl.k8s.io/$RELEASE/bin/linux/amd64/kubectl.sha256
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
--- test/run.sh
+++ test/run.sh
@@ -3,5 +3,10 @@
 POD=$(kubectl get pods -l app=package-registry-build -n zarf --output=jsonpath={.items..metadata.name})
 TEST_PATH=$(dirname "$0")
 
+while [[ "$(kubectl exec $POD -n zarf -- curl -s -o /dev/null -w ''%{http_code}'' https://github.com/.done.txt)" != "200" ]]; do
+    echo "Waiting for deployment completion"
+    sleep 5;
+done
+
 kubectl cp -n zarf $TEST_PATH/test.sh $POD:/test.sh
 kubectl exec $POD -n zarf -- /test.sh
