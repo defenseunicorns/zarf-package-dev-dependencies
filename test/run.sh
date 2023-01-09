e58baad20a4a3a76050f96bb8a8baf841a0600f9
Wayne Starr <me@racer159.com>
2023-01-09 14:44:11 -0600

    Remove erroneous semicolon

--- test/run.sh
+++ test/run.sh
@@ -5,7 +5,7 @@ TEST_PATH=$(dirname "$0")
 
 while [[ "$(kubectl exec $POD -n zarf -- curl -s -o /dev/null -w ''%{http_code}'' https://github.com/.done.txt)" != "200" ]]; do
     echo "Waiting for deployment completion"
-    sleep 5;
+    sleep 5
 done
 
 kubectl cp -n zarf $TEST_PATH/test.sh $POD:/test.sh
