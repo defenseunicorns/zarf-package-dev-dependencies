487a1c848d1f731bca0b8a8fcc449e60024ec29e
Wayne Starr <me@racer159.com>
2023-01-09 13:55:56 -0600

    Update index.sh to be more compatible

--- src/generic/index.sh
+++ src/generic/index.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 # Install kubectl from "dl.k8s.io"
-curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
-curl -LO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
+RELEASE=$(curl -L -s https://dl.k8s.io/release/stable.txt)
+echo "Installing $RELEASE"
+curl -LO https://dl.k8s.io/release/$RELEASE/bin/linux/amd64/kubectl
+curl -LO https://dl.k8s.io/$RELEASE/bin/linux/amd64/kubectl.sha256
 echo "$(cat kubectl.sha256)  kubectl" | sha256sum --check
 install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
 kubectl version --client
